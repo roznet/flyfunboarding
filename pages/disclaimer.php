aa4501e705cf23f1e58fadc7784f7824791074c8
brice <briceguard-github@yahoo.com>
2023-03-16 20:48:42 +0000

    better title

--- pages/disclaimer.php
+++ pages/disclaimer.php
@@ -51,7 +51,7 @@ $disclaimer_file = "disclaimer_{$chosen_language}.html";
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Humorous Disclaimers</title>
+<title>Fly Fun Boarding Pass Disclaimer</title>
 <style>
 body {
   font-family: Arial, sans-serif;
